fbbee0f783c2f1d5c1c50d9ce760ef5c4d6c4e89
asishkr <ashish.kumar@vidyalai.com>
2020-05-06 12:48:53 +0530

    Updates

---
+++ precache-manifest.e33b4509a901cd8ac8b25b1185181824.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969c3b2805336a3a6a4561166939aa44",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "939193bf036ce1392583",
+    "url": "/dad-jokes/static/css/main.bc2d32f6.chunk.css"
+  },
+  {
+    "revision": "ad7a62b6d8198d08bdbb",
+    "url": "/dad-jokes/static/js/2.e91e4f0e.chunk.js"
+  },
+  {
+    "revision": "939193bf036ce1392583",
+    "url": "/dad-jokes/static/js/main.af31b2cf.chunk.js"
+  },
+  {
+    "revision": "03aa9e7745c6e7ee15e7",
+    "url": "/dad-jokes/static/js/runtime~main.c81b4217.js"
+  }
+]);
